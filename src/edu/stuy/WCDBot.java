25aa37248ee2411f137bde4c4961e9945affae99
TentativelyOtherNoWangey <born2run5000@yahoo.com>
2012-01-20 20:33:16 -0500

    Started drive straight code.

--- src/edu/stuy/WCDBot.java
+++ src/edu/stuy/WCDBot.java
@@ -8,10 +8,14 @@
 package edu.stuy;
 
 
+import edu.wpi.first.wpilibj.Gyro;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PIDOutput;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.smartdashboard.SendablePIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -29,12 +33,20 @@ public class WCDBot extends IterativeRobot {
     public static final int LEFT_GEAR_SHIFT = 5;
     public static final int RIGHT_GEAR_SHIFT = 6;
 
+    public static final int GYRO_CHANNEL = 1;
     
     public static final int GAMEPAD_PORT = 1;
     
     RobotDrive drive;
     Servo leftShift, rightShift;
     Joystick gamepad;
+    Gyro gyro;
+    
+    SendablePIDController controller;
+    
+    double Kp;
+    double Ki;
+    double Kd;
     
     // TODO: implement gear switching (PWM ports 5 left and 6 right)
     
@@ -47,13 +59,25 @@ public class WCDBot extends IterativeRobot {
         leftShift = new Servo(LEFT_GEAR_SHIFT);
         rightShift = new Servo(RIGHT_GEAR_SHIFT);
         gamepad = new Joystick(GAMEPAD_PORT);
+        gyro = new Gyro(GYRO_CHANNEL);
+        controller = new SendablePIDController(Kp, Ki, Kd, gyro, new PIDOutput() {
+
+            public void pidWrite(double output) {
+                drive.arcadeDrive(-0.75, output);
+            }
+            
+        }, 0.005);
     }
 
     /**
      * This function is called periodically during autonomous
      */
     public void autonomousPeriodic() {
-
+        controller.enable();
+    }
+    
+    public void disabledInit() {
+        controller.disable();
     }
 
     /**
@@ -62,6 +86,7 @@ public class WCDBot extends IterativeRobot {
     public void teleopPeriodic() {
         drive.tankDrive(gamepad, 2, gamepad, 4);
         
+        
         if (gamepad.getRawButton(9)) {
             leftShift.set(0);
             rightShift.set(0);
@@ -69,6 +94,22 @@ public class WCDBot extends IterativeRobot {
         else if (gamepad.getRawButton(10)) {
             leftShift.set(1);
             rightShift.set(1);
+        }
+        
+        if(gamepad.getRawButton(6) && gamepad.getRawButton(8)) {
+            controller.enable();
+        }
+        else if(gamepad.getRawButton(5) && gamepad.getRawButton(7)) {
+            controller.disable();
+            drive.arcadeDrive(0, 0);
+        }
+        
+        if (gamepad.getRawButton(1)) {
+            gyro.reset();
+        }
+        
+        if (gamepad.getRawButton(2)) {
+            System.out.println(gyro.getAngle());
         }
     }
     
